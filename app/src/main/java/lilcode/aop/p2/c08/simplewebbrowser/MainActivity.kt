846632611b2029016a16bd58da20fd59566c5769
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-26 15:42:11 +0900

    Feat: 웹뷰 실행 초기에 기본 홈 구글로 로드

--- app/src/main/java/lilcode/aop/p2/c08/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2/c08/simplewebbrowser/MainActivity.kt
@@ -2,10 +2,24 @@ package lilcode.aop.p2.c08.simplewebbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://www.google.com")
     }
 }
