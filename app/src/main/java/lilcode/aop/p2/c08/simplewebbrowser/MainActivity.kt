066c6bee252c855623e792920d2956b4b010e2a7
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-26 16:04:25 +0900

    Feat: 앞,뒤로 가기 버튼 동작 구현

--- app/src/main/java/lilcode/aop/p2/c08/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2/c08/simplewebbrowser/MainActivity.kt
@@ -1,6 +1,7 @@
 package lilcode.aop.p2.c08.simplewebbrowser
 
 import android.annotation.SuppressLint
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
@@ -8,14 +9,27 @@ import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val goHomeButton: ImageButton by lazy {
+        findViewById(R.id.goHomeButton)
+    }
+
+    private val goBackButton: ImageButton by lazy {
+        findViewById(R.id.goBackButton)
+    }
+
+    private val goForwardButton: ImageButton by lazy {
+        findViewById(R.id.goForwardButton)
+    }
 
     private val webView: WebView by lazy {
         findViewById(R.id.webView)
     }
 
-    private val addressBar : EditText by lazy{
+    private val addressBar: EditText by lazy {
         findViewById(R.id.addressBar)
     }
 
@@ -37,13 +51,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun bindViews() {
+        // 주소 창
         addressBar.setOnEditorActionListener { v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_DONE){
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
                 webView.loadUrl(v.text.toString())
             }
 
             return@setOnEditorActionListener false // 키보드 닫기까지 동작 하도록
         }
 
+        // 뒤로가기 버튼
+        goBackButton.setOnClickListener {
+            webView.goBack() // 뒤로 가기
+        }
+
+        // 앞으로 가기 버튼
+        goForwardButton.setOnClickListener {
+            webView.goForward()
+        }
     }
 }
